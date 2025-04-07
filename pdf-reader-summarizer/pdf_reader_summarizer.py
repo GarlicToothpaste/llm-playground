8fa724aacc2020ad53bad4f7376e606c1e738e56
Adrian Danao <adrianpdanao@gmail.com>
2025-04-07 22:02:38 +0800

    Summarizes a page. Study embeddings in the future

--- pdf-reader-summarizer/pdf_reader_summarizer.py
+++ pdf-reader-summarizer/pdf_reader_summarizer.py
@@ -13,21 +13,24 @@ async def load_pages():
     loader = PyPDFLoader(pdf_file_path)
     pages = []
     async for page in loader.alazy_load():
+        # pages.append(page.page_content)
         pages.append(page)
+    
+    # pages = " ".join(pages)
     return pages
 
 if __name__ == "__main__":
     pages = asyncio.run(load_pages())
-    print(pdf_file_path)
-    print(pages)
+    # print(pdf_file_path)
+    # print(type(pages))
+    # print(pages[2])
 
-#Initialize the LLM
 llm = ChatOpenAI(
     model_name="llama-3.2-3b-instruct",  # Can be anything; LM Studio ignores it
     openai_api_base="http://localhost:1234/v1",  # LM Studio's endpoint
     openai_api_key="not-needed",  # Required by LangChain, but ignored by LM Studio
 )
 
-# Send a message
-response = llm([HumanMessage(content="Explain quantum physics like I'm five")])
+text = "Given the contents of this pdf file. What is the vision of the company?\n" +  pages[2].page_content
+response = llm([HumanMessage(content=text)])
 print(response.content)
