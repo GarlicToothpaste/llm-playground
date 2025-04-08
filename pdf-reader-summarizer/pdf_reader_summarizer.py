9eba93e21e1f14ddb5278c03fca009f34dbf0d15
Adrian Danao <adrianpdanao@gmail.com>
2025-04-08 08:47:25 +0800

    Added the text splitter

--- pdf-reader-summarizer/pdf_reader_summarizer.py
+++ pdf-reader-summarizer/pdf_reader_summarizer.py
@@ -1,6 +1,9 @@
 from langchain_community.document_loaders import PyPDFLoader
 import os
 import asyncio
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from typing import List #For Python 3.8 and below
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
@@ -15,22 +18,29 @@ async def load_pages():
     async for page in loader.alazy_load():
         # pages.append(page.page_content)
         pages.append(page)
-    
-    # pages = " ".join(pages)
     return pages
 
-if __name__ == "__main__":
-    pages = asyncio.run(load_pages())
-    # print(pdf_file_path)
-    # print(type(pages))
-    # print(pages[2])
+def split_documents(documents: List[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800, 
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False
+    )
+    return text_splitter.split_documents(documents)
 
-llm = ChatOpenAI(
-    model_name="llama-3.2-3b-instruct",  # Can be anything; LM Studio ignores it
-    openai_api_base="http://localhost:1234/v1",  # LM Studio's endpoint
-    openai_api_key="not-needed",  # Required by LangChain, but ignored by LM Studio
-)
 
-text = "Given the contents of this pdf file. What is the vision of the company?\n" +  pages[2].page_content
-response = llm([HumanMessage(content=text)])
-print(response.content)
+pages = asyncio.run(load_pages())
+# print(pages[0])
+chunks = split_documents(pages)
+# print(chunks[2])
+
+# llm = ChatOpenAI(
+#     model_name="llama-3.2-3b-instruct",  # Can be anything; LM Studio ignores it
+#     openai_api_base="http://localhost:1234/v1",  # LM Studio's endpoint
+#     openai_api_key="not-needed",  # Required by LangChain, but ignored by LM Studio
+# )
+
+# text = "Given the contents of this pdf file. What is the vision of the company?\n" +  pages[2].page_content
+# response = llm([HumanMessage(content=text)])
+# print(response.content)
