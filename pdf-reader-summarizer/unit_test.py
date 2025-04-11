f359cf1527276ff105cd89c5ffdda5d6b7ea0648
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-11 21:33:12 +0800

    Added another manual for testing

--- pdf-reader-summarizer/unit_test.py
+++ pdf-reader-summarizer/unit_test.py
@@ -1,6 +1,8 @@
 from query_data import query_rag
 from ollama import generate
 
+#To Run: pytest -s
+
 EVAL_PROMPT = """
 Expected Response: {expected_response}
 Actual Response: {actual_response}
@@ -8,11 +10,16 @@ Actual Response: {actual_response}
 (Answer with 'true' or 'false') Does the actual response match value of the expected response, disregard symbols? 
 """
 
-
 def test_monopoly_rules():
     assert query_and_validate(
         question="How much total money does a player start with in Monopoly? (Answer with the number only)",
         expected_response="$1500",
+    )
+
+def test_snakes_and_ladders_rules():
+    assert query_and_validate(
+        question="What square do you need to reach to win snakes and ladders (Answer with the number only)",
+        expected_response="100",
     )
 
 def query_and_validate(question: str , expected_response : str):
