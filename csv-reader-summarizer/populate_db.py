1e34a242f1a50af3e4a0ec6b81170cca125c2cd0
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-14 22:47:00 +0800

    Added insertion to ChromaDB

--- csv-reader-summarizer/populate_db.py
+++ csv-reader-summarizer/populate_db.py
@@ -1,11 +1,19 @@
 import os 
 from langchain.document_loaders import DirectoryLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from pprint import pprint
+# from pprint import pprint
 import re
+from get_embedding_function import get_embedding_function
+from langchain_chroma import Chroma
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 directory_path = dir_path + '/data'
+chroma_path = dir_path + '/chroma'
 
+def main():
+    data = load_documents()
+    add_to_chroma_db(data)
+    
 def load_documents():
     loader = DirectoryLoader(directory_path, glob='**/*.csv', loader_cls=CSVLoader)
     data = loader.load()
@@ -24,6 +32,26 @@ def calculate_chunk_ids(chunks):
 
     return chunks
         
+def add_to_chroma_db(chunks):
+    db = Chroma(persist_directory=chroma_path,embedding_function=get_embedding_function())
 
-# pprint(load_documents())
-pprint(calculate_chunk_ids(load_documents()))
+    chunks_with_id = calculate_chunk_ids(chunks)
+    existing_items = db.get()
+    existing_id = set(existing_items['ids'])
+    
+    print(f"Current Items in ChromaDB: {len(existing_id)}")
+
+    new_chunks = []
+    for chunk in chunks_with_id:
+        if chunk.metadata['id'] not in existing_id:
+            new_chunks.append(chunk)
+    
+    if len(new_chunks):
+        print(f"Items to be Added: {len(new_chunks)}")
+        new_chunk_ids=[chunk.metadata['id'] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids = new_chunk_ids)
+    else:
+        print("No New Items to be Added")
+
+if __name__ == "__main__":
+    main()
