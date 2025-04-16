51bc9915f67aa1ccb66ab9b8187d2cc493f30657
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-16 09:44:00 +0800

    Finished the function to read csv then upload it to sql db

--- sql-database-llm/csv_to_db.py
+++ sql-database-llm/csv_to_db.py
@@ -8,20 +8,26 @@ from pathlib import Path
 env_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=env_path)  
 
-url_object = URL.create(
-    drivername="mysql+pymysql",
-    username=os.getenv("LOCAL_LLM_DB_USERNAME"),
-    password=os.getenv("LOCAL_LLM_DB_PASSWORD"),
-    host=os.getenv("LOCAL_LLM_DB_HOST"),
-    port=os.getenv("LOCAL_LLM_DB_PORT"),
-    database=os.getenv("LOCAL_LLM_DB_NAME")
-)
+def main():
+    csv_path = '/home/adrian/Desktop/Personal Projects/llm-playground/sql-database-llm/data/Spotify_2024_Global_Streaming_Data.csv'
+    table_name = "spotify_streaming_2024"
+    csv_to_db (csv_path=csv_path, table_name=table_name)
 
-engine = create_engine(url_object)
+def csv_to_db(csv_path: str , table_name: str):
+    data = pd.read_csv(csv_path)
 
-df = pd.DataFrame({
-    'id': [1, 2],
-    'name': ['Alice', 'Bob']
-})
+    url_object = URL.create(
+        drivername="mysql+pymysql",
+        username=os.getenv("LOCAL_LLM_DB_USERNAME"),
+        password=os.getenv("LOCAL_LLM_DB_PASSWORD"),
+        host=os.getenv("LOCAL_LLM_DB_HOST"),
+        port=os.getenv("LOCAL_LLM_DB_PORT"),
+        database=os.getenv("LOCAL_LLM_DB_NAME")
+    )
+    engine = create_engine(url_object)
+    data.to_sql(table_name, con=engine, if_exists='replace', index=False)
 
-df.to_sql('your_table_name', con=engine, if_exists='replace', index=False)
+    return "Success"
+
+if __name__ == "__main__":
+    main()
