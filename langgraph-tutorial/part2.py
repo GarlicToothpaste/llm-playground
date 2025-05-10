2e6957dc3debada1814e8ea75d51e546321f18a0
GarlicToothpaste <adrianpdanao@gmail.com>
2025-05-10 21:18:08 +0800

    Added the llm using chatgpt and the initial stategraph|

--- langgraph-tutorial/part2.py
+++ langgraph-tutorial/part2.py
@@ -1,12 +1,32 @@
 from langchain_tavily import TavilySearch
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path(__file__).parent / ".env"
-load_dotenv(dotenv_path=env_path)  
 
 tool = TavilySearch(max_results=2)
 tools = [tool]
-# item=tool.invoke("What's a 'node' in LangGraph?")
-# print(item)
 tool.invoke("What's a 'node' in LangGraph?")
+
+import os
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model("openai:gpt-4.1")
+
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+# Modification: tell the LLM which tools it can call
+# highlight-next-line
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
