969423392cc2cc1949930813cce40142d11af322
GarlicToothpaste <adrianpdanao@gmail.com>
2025-11-26 20:21:59 +0800

    Made State Class

--- langgraph/email-customer-service-example/utils/state.py
+++ langgraph/email-customer-service-example/utils/state.py
@@ -1,3 +1,20 @@
 from typing_extensions import TypedDict, Literal
 
-class EmailAgent
+class EmailClassification(TypedDict):
+    intent: Literal["question", "bug", "billing", "feature", "complex"]
+    urgency: Literal["low", "medium", "high", "critical"]
+    topic : str 
+    summary : str 
+
+class EmailAgentState(TypedDict):
+    email_content: str
+    sender_email:str
+    email_id : str 
+
+    classification : EmailClassification | None 
+
+    search_results : list[str] | None
+    customer_history : dict | None 
+
+    draft_response : str | None 
+    messages : list[str] | None
