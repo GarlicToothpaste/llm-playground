1ddfd2cd0d6548d86aa41b75e154dd97c6c2754a
GarlicToothpaste <adrianpdanao@gmail.com>
2025-11-27 20:55:34 +0800

    Made Classification

--- langgraph/email-customer-service-example/agent.py
+++ langgraph/email-customer-service-example/agent.py
@@ -1,9 +1,11 @@
 from utils.state import EmailAgentState, EmailClassification
-from utils.nodes import read_email 
+from utils.nodes import read_email , classify_intent
 from langgraph.graph import StateGraph, START, END
 
 workflow = StateGraph(EmailAgentState)
 
 workflow.add_node("read_email", read_email)
+workflow.add_node("classify_intent", classify_intent)
 
-workflow.add_edge(START, read_email)
+workflow.add_edge(START, "read_email")
+workflow.add_edge("read_email", "classify_intent")
--- langgraph/email-customer-service-example/utils/nodes.py
+++ langgraph/email-customer-service-example/utils/nodes.py
@@ -4,7 +4,7 @@ from langgraph.types import interrupt, Command, RetryPolicy
 from typing import Literal
 from utils.state import EmailAgentState, EmailClassification
 
-model = ChatOllama(
+llm = ChatOllama(
     model="qwen3:4b",
     temperature=0.7
 )
@@ -16,5 +16,39 @@ def read_email (state : EmailAgentState):
         "messages": HumanMessage(content=f"Processing email: {state['email_content']}")
     }
 
-def classify_intent(state : EmailAgentState):
-    "To Do"
+
+# class EmailClassification(TypedDict):
+#     intent: Literal["question", "bug", "billing", "feature", "complex"]
+#     urgency: Literal["low", "medium", "high", "critical"]
+#     topic : str 
+#     summary : str 
+
+def classify_intent(state : EmailAgentState) -> Command[Literal["search_documentation", "human_review", "draft_response", "bug_tracking"]]:
+    """Use LLM to classify email intent and urgency, then route accordingly"""
+
+    structured_llm = llm.with_structured_output(EmailClassification)
+
+    classification_prompt = f"""
+    Analyze this customer email and classify it:
+
+    Email: {state['email_content']}
+    From: {state['sender_email']}
+
+    Provide classification including intent, urgency, topic, and summary.
+    """
+
+    classification = structured_llm.invoke(classification_prompt)
+
+    if classification['intent'] == 'billing' or classification['urgency'] == 'critical':
+        goto = "human_review"
+    elif classification['intent'] in ['question', 'feature']:
+        goto = "search_documentation"
+    elif classification['intent'] == 'bug':
+        goto = "bug_tracking"
+    else:
+        goto = "draft_response"
+
+    return Command(
+        update={"classification": classification},
+        goto=goto
+    )
