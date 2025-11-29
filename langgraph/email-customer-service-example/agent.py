345f559472b91989d4ab5b8b73a6cca37407c335
GarlicToothpaste <adrianpdanao@gmail.com>
2025-11-29 21:08:57 +0800

    Added Bug Tracking to the agent

--- langgraph/email-customer-service-example/agent.py
+++ langgraph/email-customer-service-example/agent.py
@@ -1,5 +1,5 @@
 from utils.state import EmailAgentState, EmailClassification
-from utils.nodes import read_email , classify_intent
+from utils.nodes import read_email , classify_intent,bug_tracking
 from langgraph.graph import StateGraph, START, END
 
 workflow = StateGraph(EmailAgentState)
@@ -9,3 +9,6 @@ workflow.add_node("classify_intent", classify_intent)
 
 workflow.add_edge(START, "read_email")
 workflow.add_edge("read_email", "classify_intent")
+
+
+workflow.add_node("bug_tracking", bug_tracking)
