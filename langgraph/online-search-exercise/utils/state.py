a9c5106b760087619aa93e9a31344c2a6583f375
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-04 21:02:31 +0800

    Started a new project to make a graph that determines which tool to use

---
+++ langgraph/online-search-exercise/utils/state.py
@@ -0,0 +1,6 @@
+from typing_extensions import TypedDict, Literal
+
+class MessageState(TypedDict):
+    message : str
+    platform_search : Literal['wikipedia' , 'duckduckgo']
+    search_result : str
