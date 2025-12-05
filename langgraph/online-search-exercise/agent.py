5be9ef8d449597af471aad72aed7addf68083e42
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-05 20:55:28 +0800

    Added Classifications and more nodes

--- langgraph/online-search-exercise/agent.py
+++ langgraph/online-search-exercise/agent.py
@@ -1,20 +1,20 @@
 from langgraph.graph import StateGraph, START, END
 from utils.state import MessageState
-from utils.nodes import read_message
+from utils.nodes import read_message, classify_message
 
 workflow = StateGraph(MessageState)
 
 workflow.add_node("read_message", read_message)
+workflow.add_node("classify_message", classify_message)
 workflow.add_edge(START, "read_message")
-workflow.add_edge("read_message", END)
+workflow.add_edge("read_message", "classify_message")
+# workflow.add_edge("read_message", END)
+
 
 app = workflow.compile()
 
 initial_state = {
     "message_content": "This is a test message"
-    # "sender_email": "customer@example.com",
-    # "email_id": "email_123",
-    # "messages": []
 }
 
 result = app.invoke(initial_state)
--- langgraph/online-search-exercise/utils/state.py
+++ langgraph/online-search-exercise/utils/state.py
@@ -1,6 +1,9 @@
 from typing_extensions import TypedDict, Literal
 
+class MessageClassification(TypedDict):
+    platform : Literal['wikipedia', 'duckduckgo']
+
 class MessageState(TypedDict):
     message_content : str
-    platform_search : Literal['wikipedia' , 'duckduckgo'] | None
+    classification : MessageClassification | None
     search_result : str | None
