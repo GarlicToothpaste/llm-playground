b57bccc2cf8ae16ebe31b44d6e431ca4573ceecf
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-07 21:27:20 +0800

    added tools

---
+++ langgraph/online-search-exercise/utils/tools.py
@@ -0,0 +1,18 @@
+from langchain.tools import tool
+from langchain_community.retrievers import WikipediaRetriever
+from langchain_community.tools import DuckDuckGoSearchRun
+
+@tool
+def search_wikipedia(word : str):
+    """Searches Wikipedia given a string as a keyword"""
+    wiki = WikipediaRetriever()
+    docs = wiki.invoke(word)
+    information = "\n---\n".join([f"**{doc.metadata['title']}**\n{doc.page_content}" for doc in docs])
+    return information
+
+@tool
+def search_internet(word : str):
+    """Searches the internet given a string as a keyword"""
+    search = DuckDuckGoSearchRun()
+    information = search.invoke(word)
+    return information
