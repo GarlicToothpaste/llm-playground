07b7b7a72247c1ede9cba4d727c555cabd45c2a8
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-09 20:53:23 +0800

    Summarize Search

--- langgraph/online-search-exercise/agent.py
+++ langgraph/online-search-exercise/agent.py
@@ -1,13 +1,17 @@
 from langgraph.graph import StateGraph, START, END
 from utils.state import MessageState
-from utils.nodes import read_message, classify_message, duckduckgo_search, wikipedia_search
+from utils.nodes import read_message, classify_message, duckduckgo_search, wikipedia_search, summarize_search
 
 workflow = StateGraph(MessageState)
 
+#Nodes
 workflow.add_node("read_message", read_message)
 workflow.add_node("classify_message", classify_message)
 workflow.add_node("duckduckgo_search", duckduckgo_search)
 workflow.add_node("wikipedia_search", wikipedia_search)
+workflow.add_node("summarize_search", summarize_search)
+
+#Edges
 workflow.add_edge(START, "read_message")
 workflow.add_edge("read_message", "classify_message")
 workflow.add_edge("classify_message", END)
