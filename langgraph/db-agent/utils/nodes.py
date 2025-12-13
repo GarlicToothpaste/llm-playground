7ac83c5b6fb060aa49174bd4bbfd0bfae24d7ecb
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-13 20:33:59 +0800

    Made the initial structure for the nodes

---
+++ langgraph/db-agent/utils/nodes.py
@@ -0,0 +1,31 @@
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model="qwen3:4b",
+    temperature=0.7
+)
+
+#TODO: Read User Input
+def read_message():
+    print("TEST")
+    
+#TODO: Classify Which Operation to use Based on the User Input
+def classify_message():
+    print("TEST")
+
+#TODO: Add Items to the Database
+def add_item():
+    print("TEST")
+
+#TODO: Update Item Information in the Database
+def update_item():
+    print("TEST")
+
+#TODO: Generate the Notification of Changes to the User
+def generate_update_notification():
+    print("TEST")
+
+
+#TODO: Send Update Message to the User
+def send_update_message():
+    print("TEST")
