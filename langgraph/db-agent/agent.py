80724f3e2c85c7ec32db6c1d3e29f2e82e1cddf8
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-13 20:38:56 +0800

    Added the draft flow to the agent

--- langgraph/db-agent/agent.py
+++ langgraph/db-agent/agent.py
@@ -1,4 +1,13 @@
 from langgraph.graph import StateGraph, START, END
 from utils.state import AgentState
+from utils.nodes import read_message, classify_message, add_item, update_item, generate_update_notification, send_update_message
 
 workflow = StateGraph(AgentState)
+
+workflow.add_node("read_meessage", read_message)
+workflow.add_node("classify_message", classify_message)
+workflow.add_node("add_item", add_item)
+workflow.add_node("update_item", update_item)
+workflow.add_node("generate_update_notification", generate_update_notification)
+workflow.add_node("send_update_message", send_update_message)
+
--- langgraph/db-agent/utils/nodes.py
+++ langgraph/db-agent/utils/nodes.py
@@ -7,25 +7,25 @@ llm = ChatOllama(
 
 #TODO: Read User Input
 def read_message():
-    print("TEST")
-    
+    print("TEST Read Message")
+
 #TODO: Classify Which Operation to use Based on the User Input
 def classify_message():
-    print("TEST")
+    print("TEST Test Classify Message")
 
 #TODO: Add Items to the Database
 def add_item():
-    print("TEST")
+    print("TEST Classify Add_Item")
 
 #TODO: Update Item Information in the Database
 def update_item():
-    print("TEST")
+    print("TEST Classify Update Item")
 
 #TODO: Generate the Notification of Changes to the User
 def generate_update_notification():
-    print("TEST")
+    print("TEST Generate Update Notif")
 
 
 #TODO: Send Update Message to the User
 def send_update_message():
-    print("TEST")
+    print("TEST Send Update Message")
