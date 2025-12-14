599c81099d62f1bbf749fee258df34a9bd76efde
GarlicToothpaste <adrianpdanao@gmail.com>
2025-12-14 20:38:08 +0800

    Added classification node

--- langgraph/db-agent/utils/nodes.py
+++ langgraph/db-agent/utils/nodes.py
@@ -1,20 +1,46 @@
 from langchain_ollama import ChatOllama
 from langchain.messages import HumanMessage
-from utils.state import AgentState
+from utils.state import AgentState, OperationClassification
+from langgraph.types import Command
+
 llm = ChatOllama(
     model="qwen3:4b",
     temperature=0.7
 )
 
 def read_message(state: AgentState):
-    print (state['message_content'])
+    """Reads the message input of the user"""
     return {
         "messages": HumanMessage(content=f"Processing message: {state['message_content']}")
     }
 
 #TODO: Classify Which Operation to use Based on the User Input
-def classify_message():
-    print("TEST Test Classify Message")
+def classify_message(state: AgentState):
+    """Use the LLM to classify which tool to use"""
+
+    structured_llm = llm.with_structured_output(OperationClassification)
+
+    classification_prompt = f"""
+    Analyze this message and classify which sql function it will use
+
+    Message: {state['message_content']}
+
+    Classify the sql function
+    """
+
+    classification = structured_llm.invoke(classification_prompt)
+    # print(classification)
+
+    if classification['operation'] == "add_item":
+        goto = "add_item"
+    if classification['operation'] == "update_item":
+        goto = "update_item"
+
+    print(classification['operation'])
+    return Command(
+        update = {"classification" : classification},
+        goto = goto
+    )
 
 #TODO: Add Items to the Database
 def add_item():
