dd77a2a5e00a6defa61de88f5997d57434d2a950
GarlicToothpaste <adrianpdanao@gmail.com>
2026-01-06 20:15:40 +0800

    Added Retry Policy for the SQL Related Nodes

--- langgraph/db-agent/agent.py
+++ langgraph/db-agent/agent.py
@@ -1,15 +1,16 @@
 from langgraph.graph import StateGraph, START, END
 from utils.state import AgentState
 from utils.nodes import read_message, classify_message, add_item, update_item, generate_update_notification, send_update_message, show_items
+from langgraph.types import RetryPolicy
 
 workflow = StateGraph(AgentState)
 
 #Nodes
 workflow.add_node("read_message", read_message)
 workflow.add_node("classify_message", classify_message)
-workflow.add_node("show_items", show_items)
-workflow.add_node("add_item", add_item)
-workflow.add_node("update_item", update_item)
+workflow.add_node("show_items", show_items, retry_policy=RetryPolicy(max_attempts=3, initial_interval=0.5))
+workflow.add_node("add_item", add_item, retry_policy=RetryPolicy(max_attempts=3, initial_interval=0.5))
+workflow.add_node("update_item", update_item, retry_policy=RetryPolicy(max_attempts=3, initial_interval=0.5))
 workflow.add_node("generate_update_notification", generate_update_notification)
 workflow.add_node("send_update_message", send_update_message)
 
