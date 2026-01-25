24233a94f261d91c39af586f9528489225777eba
GarlicToothpaste <adrianpdanao@gmail.com>
2026-01-25 18:12:17 +0800

    installed the necessary dependencies

---
+++ langchain/voice_agent/app.py
@@ -0,0 +1,5 @@
+#Using the example on Langchain: https://docs.langchain.com/oss/python/langchain/voice-agent
+# Repo Link: https://github.com/langchain-ai/voice-sandwich-demo#readme
+from typing import AsyncIterator
+import asyncio
+import assemblyai as aai #Refer to Docs. Code in langchain is outdated. https://www.assemblyai.com/docs/pre-recorded-audio
