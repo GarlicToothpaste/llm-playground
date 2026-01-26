1624b802df81d939e4dcf96b577539374300710f
GarlicToothpaste <adrianpdanao@gmail.com>
2026-01-26 20:34:19 +0800

    Added speech models

--- assembly-ai/quickstart/app.py
+++ assembly-ai/quickstart/app.py
@@ -8,7 +8,10 @@ aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
 
 audio_file = "https://assembly.ai/wildfires.mp3"
 
-transcript = aai.Transcriber().transcribe(audio_file)
+# transcript = aai.Transcriber().transcribe(audio_file)
+
+config = aai.TranscriptionConfig(speech_models=["slam-1", "universal" ])
+transcript = aai.Transcriber(config=config).transcribe(audio_file)
 
 if transcript.status == "error":
   raise RuntimeError(f"Transcription failed: {transcript.error}")
