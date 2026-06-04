e75cc8a4424d7f625b2589dce30373305390df8c
GarlicToothpaste <adrianpdanao@gmail.com>
2026-06-04 22:18:02 +0800

    Finalized agents

--- langgraph/workflows+agents/agents.py
+++ langgraph/workflows+agents/agents.py
@@ -42,3 +42,73 @@ tools = [add, multiply, divide]
 tools_by_name = {tool.name: tool for tool in tools}
 llm_with_tools = llm.bind_tools(tools)
 
+from langgraph.graph import MessagesState
+from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
+from typing_extensions import Literal , TypedDict
+from langgraph.graph import StateGraph, START, END
+
+
+def llm_call(state: MessagesState):
+    """LLM decides whether to call a tool or not."""
+
+    # Ensure the input to invoke is a standard list of message objects
+    system_message = SystemMessage(
+        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+    )
+    
+    # Prepend the system message to the current chat history
+    messages = [system_message] + state["messages"]
+    
+    return {
+        "messages": [llm_with_tools.invoke(messages)]
+    }
+
+def tool_node(state:MessagesState):
+    """Performs the tool call"""
+    
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call['args'])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+def should_continue(state:MessagesState) -> Literal['tool_node', END]:
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    if last_message.tool_calls:
+        return "tool_node"
+    
+    return END
+
+# Build workflow
+agent_builder = StateGraph(MessagesState)
+
+# Add nodes
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_conditional_edges(
+    "llm_call",
+    should_continue,
+    ["tool_node", END]
+)
+agent_builder.add_edge("tool_node", "llm_call")
+
+# Compile the agent
+agent = agent_builder.compile()
+
+# Show the agent
+agent_builder_graph = agent.get_graph().draw_mermaid_png()
+with open("agent_builder.png", "wb") as f:
+    f.write(agent_builder_graph)
+
+# Invoke
+messages = [HumanMessage(content="Add 3 and 4.")]
+messages = agent.invoke({"messages": messages})
+for m in messages["messages"]:
+    m.pretty_print()
